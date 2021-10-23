20eb3da335860ad5a02200b55e88a6fa27672eef
brendamuthonikamau <85281618+brendamuthonikamau@users.noreply.github.com>
2021-10-23 12:36:04 +0300

    Update phylogenetic.sh

--- workflow/scripts/phylogenetic.sh
+++ workflow/scripts/phylogenetic.sh
@@ -3,8 +3,11 @@
 # step 1:mafft used for sequence alignment
 mafft
 
-# step 2:iqtree used for tree generation
+# step 2:aliview used for sequence curation and inspection
+aliview aligned_seq_final.fasta
+
+# step 3:iqtree used for tree generation
 iqtree -s aligned_seq_final.fasta -m MFP -B 1000
 
-# step 3:figtree used for tree visualization
+# step 4:figtree used for tree visualization
 figtree aligned_seq_final.fasta.treefile
