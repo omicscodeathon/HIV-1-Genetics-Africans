bf9e77560247e69e94a5e5743d2f38cc1b562d9e
Kauthar M. Omar <57720624+Kauthar-Omar@users.noreply.github.com>
2021-10-25 11:53:55 +0300

    Update phylogenetic.sh

--- workflow/scripts/phylogenetic.sh
+++ workflow/scripts/phylogenetic.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
+#NB: Ensure to merge the reference sequences with query sequences before running the script.
 
 # step 1:mafft used for sequence alignment
-mafft
+mafft --quiet sequences.fasta > aligned_seq_final.fasta
 
 # step 2:aliview used for sequence curation and inspection
 aliview aligned_seq_final.fasta
